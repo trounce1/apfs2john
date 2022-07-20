ac39445d6d9a0aa17601c2ee52efda9eb1546398
TROUNCE <trounceharry@gmail.com>
2022-07-20 13:33:06 +0100

    Update Unicode.h

--- ApfsLib/Unicode.h
+++ ApfsLib/Unicode.h
@@ -2,7 +2,7 @@
 
 #include <cstdint>
 #include <vector>
-#include <stddef>
+#include <stddef.h>
 
 int normalizeOptFoldU32Char(char32_t ch, bool case_insensitive, char32_t *sequence_out, unsigned char *unknown_out);
 void CanonicalReorder(char32_t *nfd, uint8_t *ccc, size_t len);
